1d4ddda9f71ff37b44b376bbfe997bbb73349ce4
Tsvetozar Bonev <tsbonev@gmail.com>
2018-05-16 17:54:03 +0300

    Add failing scenarios

--- tests/com/clouway/crm/core/messaging_system/MessengerTest.java
+++ tests/com/clouway/crm/core/messaging_system/MessengerTest.java
@@ -1,6 +1,7 @@
 package com.clouway.crm.core.messaging_system;
 
 import com.google.common.base.Strings;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.jmock.Mockery;
 import org.jmock.Expectations;
@@ -15,8 +16,8 @@ class MessengerTest {
     @Test
     public void acceptValidMessage(){
 
-        String title = "Message title";
-        String content = "This is a message!";
+        final String title = "Message title";
+        final String content = "This is a message!";
 
         assertCorrectMessage(title, content);
 
@@ -24,16 +25,18 @@ class MessengerTest {
 
     @Test
     public void refuseInvalidMessage(){
-        Message message = context.mock(Message.class);
 
-        String title = "Message title";
-        StringBuilder stringBuilder = new StringBuilder();
+        final Message message = context.mock(Message.class);
+
+        final String title = "";
+        final StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(Strings.repeat("1", 121));
-        String invalidContent = stringBuilder.toString();
+        final String invalidContent = stringBuilder.toString();
 
         context.checking(new Expectations(){{
 
-            oneOf(message).isValid(title, invalidContent); will(returnValue(invalidContent.matches(".{1,120}")));
+            oneOf(message).isValid(title, invalidContent); will(returnValue(!Strings.isNullOrEmpty(title)
+                    && invalidContent.matches(".{1,120}")));
 
         }});
 
@@ -44,35 +47,78 @@ class MessengerTest {
     }
 
     @Test
-    public void sendValidMessage(){
+    public void sendMessageToNullReceiver(){
 
-        String title = "Message title";
-        String content = "This is a message!";
+        final Message message = context.mock(Message.class);
+        final Receiver receiver = null;
+        final Messenger messenger = context.mock(Messenger.class);
 
-        Message message = context.mock(Message.class);
-        Messenger messenger = context.mock(Messenger.class);
-        Receiver receiver = context.mock(Receiver.class);
+        context.checking(new Expectations(){{
+
+            oneOf(messenger).sendMessage(receiver, message); will(returnValue(receiver != null));
+
+        }});
+
+        assertThat(messenger.sendMessage(receiver, message), is(false));
+
+        context.assertIsSatisfied();
+
+    }
+
+    @Test
+    public void sendNullMessageToReceiver(){
+
+        final Message message = null;
+        final Receiver receiver = context.mock(Receiver.class);
+        final Messenger messenger = context.mock(Messenger.class);
+
+        context.checking(new Expectations(){{
+
+            oneOf(messenger).sendMessage(receiver, message); will(returnValue(message != null));
+            oneOf(receiver).receiveMessage(message); will(returnValue(message == null ? null : "some message"));
+
+        }});
+
+        assertThat(messenger.sendMessage(receiver, message), is(false));
+        assertThat(receiver.receiveMessage(message), is(equalTo(null)));
+
+        context.assertIsSatisfied();
+
+    }
+
+    @Test
+    public void sendAndReceiveValidMessage(){
+
+        final String title = "Message title";
+        final String content = "This is a message!";
+
+        final Message message = context.mock(Message.class);
+        final Messenger messenger = context.mock(Messenger.class);
+        final Receiver receiver = context.mock(Receiver.class);
 
         context.checking(new Expectations(){{
 
             oneOf(message).isValid(title, content); will(returnValue(content.matches(".{1,120}")));
             oneOf(messenger).sendMessage(receiver, message); will(returnValue(receiver != null));
-
+            oneOf(receiver).receiveMessage(message); will(returnValue(title + " " + content));
         }});
 
-        message.isValid(title, content);
-        messenger.sendMessage(receiver, message);
+        assertThat(message.isValid(title, content), is(true));
+        assertThat(messenger.sendMessage(receiver, message), is(true));
+        assertThat(receiver.receiveMessage(message), is(equalTo(title + " " + content)));
 
         context.assertIsSatisfied();
 
     }
 
     private void assertCorrectMessage(String title, String content){
-        Message message = context.mock(Message.class);
+
+        final Message message = context.mock(Message.class);
 
         context.checking(new Expectations(){{
 
-            oneOf(message).isValid(title, content); will(returnValue(content.matches(".{1,120}")));
+            oneOf(message).isValid(title, content); will(returnValue(!Strings.isNullOrEmpty(title)
+                    && content.matches(".{1,120}")));
 
         }});
 
